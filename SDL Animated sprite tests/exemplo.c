42bd16844409a999e7a07c0ec20fdbc398bb68c2
Wellington Domiciano <wldomiciano@gmail.com>
2019-12-22 10:40:02 -0300

    Add missing include

--- SDL Animated sprite tests/exemplo.c
+++ SDL Animated sprite tests/exemplo.c
@@ -1,57 +1,59 @@
+#include <SDL.h>
+
 struct SpriteSheet {
-    SDL_Texture* texture;
-    int quantRegions;
-    SDL_Rect* regions;
+  SDL_Texture* texture;
+  int quantRegions;
+  SDL_Rect* regions;
 };
 
 struct Sprite {
-    SpriteSheet* sheet;
-    int region;
-    double angle;
-    SDL_Point position;
-    SDL_Point scale;
-    SDL_Point center;
-    SDL_RendererFlip flip;
+  SpriteSheet* sheet;
+  int region;
+  double angle;
+  SDL_Point position;
+  SDL_Point scale;
+  SDL_Point center;
+  SDL_RendererFlip flip;
 };
 
 struct AnimatedSprite {
-    Sprite* sprite;
-    int quantAnimations;
-    int currentAnimation;
-    Animation* animations;
+  Sprite* sprite;
+  int quantAnimations;
+  int currentAnimation;
+  Animation* animations;
 };
 
 struct Animation {
-    float speed;
-    int currentFrame;
-    int quantFrames;
-    int* frames;
+  float speed;
+  int currentFrame;
+  int quantFrames;
+  int* frames;
 };
 
 Animation* getCurrentAnimationPointer(AnimatedSprite* sprite) {
-    return &sprite->animations[ sprite->currentAnimation ];
+  return &sprite->animations[sprite->currentAnimation];
 }
 
 int getCurrentAnimation(AnimatedSprite* sprite) {
-    return sprite->currentAnimation;
+  return sprite->currentAnimation;
 }
 
 Animation* getAnimation(AnimatedSprite* sprite, int animationIndex) {
-    return &sprite->animations[ animationIndex ];
+  return &sprite->animations[animationIndex];
 }
 
-int main(int argc, char const *argv[]) {
-    int image = loadImage("path.jpg");
+int main(int argc, char const* argv[]) {
+  int image = loadImage("path.jpg");
 
-    SpriteSheet* sheet = createSpriteSheet("/path/to/image.jpg");
-    Sprite* player = createSprite(sheet, 0);
+  SpriteSheet* sheet = createSpriteSheet("/path/to/image.jpg");
+  Sprite* player = createSprite(sheet, 0);
 
-    AnimatedSprite* player = createAnimatedSprite(sheet, 3);
-    int playerWalk = createAnimation(player, 4, 1.0, 0, 1, 2, 3);
-    int playerJump = createAnimation(player, 5, 1.0, 4, 5, 6, 7, 8);
-    int playerAttack = createAnimation(player, 4, 1.5, 9, 10, 11, 12);
+  AnimatedSprite* player = createAnimatedSprite(sheet, 3);
+  int playerWalk = createAnimation(player, 4, 1.0, 0, 1, 2, 3);
+  int playerJump = createAnimation(player, 5, 1.0, 4, 5, 6, 7, 8);
+  int playerAttack = createAnimation(player, 4, 1.5, 9, 10, 11, 12);
 
-    playAnimation(player, playerWalk);
+  playAnimation(player, playerWalk);
 
-    return 0;
+  return 0;
 }
