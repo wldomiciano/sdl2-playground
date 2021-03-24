546eb3f7e0b348ffd2bf5277181c2d489d407e42
Wellington Domiciano <wldomiciano@gmail.com>
2021-03-24 15:31:15 -0300

    Improve project

--- projects/fire-to-mouse-direction/main.c
+++ projects/fire-to-mouse-direction/main.c
@@ -1,71 +1,77 @@
-#include "utils.h"
-
-const double PI = 3.14159265358979323846264338327950288;
+#include "context.h"
+#include "events.h"
+#include "initialization.h"
+#include "math.h"
+#include "miscellaneous.h"
+#include "sprite.h"
+#include "vector.h"
 
 int main() {
-  create_game(400, 400);
-  SDL_Rect screen = {-10, -10, 420, 420};
-  SDL_Texture* ship = load_texture_from_file("./assets/images/image.bmp");
-  SDL_Texture* bullet = load_texture_from_file("./assets/images/bullet.bmp");
+  initializeAllWithContext(0, 0, "Hello", 400, 400, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 0, SDL_RENDERER_PRESENTVSYNC);
 
-  SDL_Rect shipRect = {175, 175, 50, 50};
-  SDL_Rect bulletRect = {shipRect.x + shipRect.w / 2 - 5, shipRect.y - 25, 10,
-                         20};
-  SDL_Point bulletCenter = {bulletRect.w / 2, shipRect.h};
-  double angle = 0;
-  double bulletAngle = 0;
+  Sprite* const ship = createSprite(NULL, "/assets/images/image.bmp", NULL);
+  Sprite* const bullet = createSprite(NULL, "/assets/images/bullet.bmp", NULL);
 
-  double velocityX = 0;
-  double velocityY = 0;
-  int shipSpeed = 3;
-  int bulletSpeed = 6;
   SDL_bool ready = SDL_TRUE;
-  float directionX = 0, directionY = 0;
-  while (is_running()) {
-    handle_input();
+  const int shipSpeed = 3;
+  const int bulletSpeed = 6;
 
-    if (SDL_GetKeyboardState(NULL)[SDL_SCANCODE_A]) {
-      shipRect.x += -shipSpeed;
-    } else if (SDL_GetKeyboardState(NULL)[SDL_SCANCODE_D]) {
-      shipRect.x += shipSpeed;
-    } else if (SDL_GetKeyboardState(NULL)[SDL_SCANCODE_W]) {
-      shipRect.y += -shipSpeed;
-    } else if (SDL_GetKeyboardState(NULL)[SDL_SCANCODE_S]) {
-      shipRect.y += shipSpeed;
+  setSpriteOrigin(bullet, VEC2(5, 20));
+  setSpritePosition(bullet, VEC2(200, 120));
+  setSpriteGlogalPivot(bullet, VEC2(200, 200));
+
+  setSpriteOrigin(ship, VEC2(25, 25));
+  setSpritePosition(ship, VEC2(200, 200));
+  setSpriteGlogalPivot(ship, VEC2(200, 200));
+
+  float bulletRot = 0;
+  vec2 bulletVel = {0, 0};
+
+  while (wasQuitNotRequested()) {
+    handleEvents();
+
+    if (isKeyPressed(SDL_SCANCODE_A)) {
+      moveSprite(ship, VEC2(-shipSpeed, 0));
+      if (ready) moveSprite(bullet, VEC2(-shipSpeed, 0));
+    } else if (isKeyPressed(SDL_SCANCODE_D)) {
+      moveSprite(ship, VEC2(shipSpeed, 0));
+      if (ready) moveSprite(bullet, VEC2(shipSpeed, 0));
     }
 
-    int mouseX, mouseY;
-    SDL_GetMouseState(&mouseX, &mouseY);
-    int delta_x = (shipRect.x + shipRect.w / 2) - mouseX;
-    int delta_y = (shipRect.y + shipRect.h / 2) - mouseY;
-    double rad = SDL_atan2(delta_x, delta_y);
-    angle = -rad * 180.0 / PI;
+    if (isKeyPressed(SDL_SCANCODE_W)) {
+      moveSprite(ship, VEC2(0, -shipSpeed));
+      if (ready) moveSprite(bullet, VEC2(0, -shipSpeed));
+    } else if (isKeyPressed(SDL_SCANCODE_S)) {
+      moveSprite(ship, VEC2(0, shipSpeed));
+      if (ready) moveSprite(bullet, VEC2(0, shipSpeed));
+    }
+
+    const vec2 delta = sub(getSpritePosition(ship), getMousePosition());
+    const double rot = rotation(delta);
+    setSpriteRotation(ship, -rot);
 
     if (ready) {
-      bulletAngle = angle;
-      directionX = SDL_sinf(rad);
-      directionY = SDL_cosf(rad);
+      bulletRot = rot;
+      bulletVel = mul(normal(VEC2(-delta.x, -delta.y)), bulletSpeed);
+      setSpriteRotation(bullet, -bulletRot);
       ready = SDL_FALSE;
     }
-    velocityX = directionX * bulletSpeed;
-    velocityY = directionY * bulletSpeed;
-    bulletRect.x -= velocityX;
-    bulletRect.y -= velocityY;
-    if (!SDL_HasIntersection(&bulletRect, &screen)) {
-      bulletRect.x = shipRect.x + shipRect.w / 2 - 5;
-      bulletRect.y = shipRect.y - 25;
-      velocityX = velocityY = 0;
+
+    moveSprite(bullet, bulletVel);
+
+    const vec2 pos = getSpritePosition(bullet);
+    if (pos.x > 450 || pos.x < -50 || pos.y > 450 || pos.y < -50) {
+      const vec2 pos = getSpritePosition(ship);
+      setSpritePosition(bullet, pos);
+      setSpriteGlogalPivot(bullet, pos);
       ready = SDL_TRUE;
     }
 
-    render_clear();
-    SDL_RenderCopyEx(renderer, ship, NULL, &shipRect, angle, NULL,
-                     SDL_FLIP_NONE);
-    SDL_RenderCopyEx(renderer, bullet, NULL, &bulletRect, bulletAngle,
-                     &bulletCenter, SDL_FLIP_NONE);
-    render_present();
+    clearRender(NULL, (SDL_Color){0, 0, 0, 255});
+    drawSprite(bullet);
+    drawSprite(ship);
+    presentRender(NULL);
   }
 
-  destroy_game();
   return 0;
 }
