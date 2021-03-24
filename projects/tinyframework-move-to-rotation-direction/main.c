eb68a937769374471e018b3de6de18e1658dfc9e
Wellington Domiciano <wldomiciano@gmail.com>
2021-03-24 17:07:01 -0300

    Change project name

---
+++ projects/tinyframework-move-to-rotation-direction/main.c
@@ -0,0 +1,50 @@
+#include "tinyframework.h"
+
+int main() {
+  initializeAllWithContext(0, 0, "Hello", 400, 400, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 0, SDL_RENDERER_PRESENTVSYNC);
+
+  Sprite* const ship = createSprite(NULL, "/assets/images/image.bmp", NULL);
+
+  Uint32 time = SDL_GetTicks();
+
+  const int angleSpeed = 15;
+
+  const int speed = 5;
+
+  vec2 velocity = {0, 0};
+
+  setSpriteOrigin(ship, VEC2(25, 25));
+  setSpritePivot(ship, VEC2(25, 25));
+  setSpritePosition(ship, VEC2(200, 200));
+
+  while (wasQuitNotRequested()) {
+    handleEvents();
+
+    if (isKeyPressed(SDL_SCANCODE_RIGHT) && SDL_GetTicks() > time + 200) {
+      rotateSprite(ship, angleSpeed);
+      time = SDL_GetTicks();
+    } else if (isKeyPressed(SDL_SCANCODE_LEFT) && SDL_GetTicks() > time + 200) {
+      rotateSprite(ship, -angleSpeed);
+      time = SDL_GetTicks();
+    }
+
+    if (isKeyPressed(SDL_SCANCODE_UP)) {
+      float rad = deg2rad(getSpriteAngle(ship));
+
+      const vec2 direction = {SDL_sinf(rad), -SDL_cosf(rad)};
+
+      velocity = mul(direction, speed);
+    } else if (isKeyPressed(SDL_SCANCODE_DOWN)) {
+      setSpritePosition(ship, VEC2(200, 200));
+      velocity = VEC2(0, 0);
+    }
+
+    moveSprite(ship, velocity);
+
+    clearRender(NULL, (SDL_Color){0, 0, 0, 255});
+    drawSprite(ship);
+    presentRender(NULL);
+  }
+
+  return 0;
+}
