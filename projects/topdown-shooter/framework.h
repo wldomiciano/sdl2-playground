df0a35c83bd66939998f69fe0edb781ebe51a19f
Wellington Domiciano <wldomiciano@gmail.com>
2021-04-08 01:26:40 -0300

    Add extern keyword

--- projects/topdown-shooter/framework.h
+++ projects/topdown-shooter/framework.h
@@ -8,15 +8,15 @@ typedef void (*stateFn)();
 typedef struct Game {
   SDL_Rect windowFrame;
 
-  SDL_Window* window;
+  SDL_Window*   window;
   SDL_Renderer* renderer;
-  SDL_bool isRunning;
-  stateFn create;
-  stateFn update;
-  stateFn destroy;
+  SDL_bool      isRunning;
+  stateFn       create;
+  stateFn       update;
+  stateFn       destroy;
 } Game;
 
-const double PI;
+extern const double PI;
 
 int game_run(const char* const title, const Uint16 width, const Uint16 height,
              stateFn create, stateFn update, stateFn destroy);
