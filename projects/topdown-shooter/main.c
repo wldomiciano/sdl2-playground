db76dbebda0be30bf7eba797cf918fb9a0379e89
Wellington Domiciano <wldomiciano@gmail.com>
2021-04-09 18:27:52 -0300

    Remove unused headers

--- projects/topdown-shooter/main.c
+++ projects/topdown-shooter/main.c
@@ -1,6 +1,6 @@
-#include <SDL.h>
-#include <stdarg.h>
 #include "framework.h"
 #include "game.h"
 
-int main(int argc, char** argv) { return game_run("Hello", 400, 400, create, update, destroy); }
+int main(int argc, char** argv) {
+  return game_run("Hello", 400, 400, create, update, destroy);
+}
