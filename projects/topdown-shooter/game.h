fdd344985593e9db87ec1396ec8e1ed6f97dbe57
Wellington Domiciano <wldomiciano@gmail.com>
2021-04-09 18:49:55 -0300

    Remove unused files

--- projects/topdown-shooter/game.h
+++
@@ -1,8 +0,0 @@
-#ifndef GAME_H
-#define GAME_H
-
-void create();
-void update();
-void destroy();
-
-#endif
--- projects/topdown-shooter/main.c
+++ projects/topdown-shooter/main.c
@@ -1,5 +1,19 @@
 #include "framework.h"
-#include "game.h"
+#include "player.h"
+
+static void create() {
+  player_create();
+  SDL_Log("create");
+}
+
+static void update() {
+  player_update();
+}
+
+static void destroy() {
+  player_destroy();
+  SDL_Log("destroy");
+}
 
 int main(int argc, char** argv) {
   return game_run("Hello", 400, 400, create, update, destroy);
